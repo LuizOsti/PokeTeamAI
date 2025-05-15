85e536ed25a6838d32df38414e6ca97d1222e7df
Luiz Felipe Osti <lfelipe.osti@gmail.com>
2025-05-15 17:13:17 -0300

    Corrigido erros do commit

--- src/main/java/Peixo/IAdeTreinoPOKE/repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package Peixo.IAdeTreinoPOKE.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Peixo.IAdeTreinoPOKE.model.TeamModel;
-
-public interface TeamRepository extends JpaRepository<TeamModel, Integer> {
-}
